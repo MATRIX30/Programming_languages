ac10300aec8ce00c30a2dafd57adc9f1771a016c
Matrix30 <matrixboy30@gmail.com>
2024-11-26 23:19:44 +0100

    finishing with account

---
+++ Ruby/classes/account.rb
@@ -0,0 +1,16 @@
+# class to manage accounts
+
+class Account 
+  def initialize(balance)
+    @balance = balance
+  end
+
+  def balance(new_balance):
+    @balance = new_balance
+    
+  end
+
+  def balance
+    @balance
+  end
+end
