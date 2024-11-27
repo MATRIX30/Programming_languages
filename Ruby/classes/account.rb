7d27a3d59be83ed405682d6bca9e23eaef93367b
Matrix30 <matrixboy30@gmail.com>
2024-11-28 00:19:28 +0100

    add OOP account code

--- Ruby/classes/account.rb
+++ Ruby/classes/account.rb
@@ -1,16 +1,30 @@
 # class to manage accounts
 
 class Account 
-  def initialize(balance)
+  def initialize(balance = 0) # create a constructor and default value to 0
     @balance = balance
   end
 
-  def balance(new_balance):
+  def balance=(new_balance)
     @balance = new_balance
-    
   end
 
   def balance
     @balance
   end
+  #class variable for bank
+  @@bank_name = "My Bank"
+
+  def self.bank_name
+    @@bank_name
+  end
+  
+  # implicit setters and getter for balance
+  # attr_accessor :balance
 end
+
+
+
+new_account = Account.new(1000)
+print(Account.bank_name)
+print(Account.balance = 2000)
